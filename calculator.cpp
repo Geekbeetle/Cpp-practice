0a971cbb7ef5f9bdc3e02daccaf96f70f2235f82
unknown <sant72670@gmail.com>
2026-09-25 22:59:15 +0530

    added while loop to continue

--- calculator.cpp
+++ calculator.cpp
@@ -1,29 +1,39 @@
 #include <iostream>
 int main(){
     std::cout<<"This is a simple calculator "<<'\n';
-    double a,b;
-    std::cout<<"Enter the first number: ";
-    std::cin>>a;
-    std::cout<<"Enter the second number: ";
-    std::cin>>b;
-    char op;
-    std::cout<<"enter operation(+,-,*,/): ";
-    std::cin>>op;
-    switch(op){
-        case '+':
-        std::cout<<"the sum is: "<< a+b<<'\n';
+    while (true) {
+        double a,b;
+        std::cout<<"Enter the first number: ";
+        std::cin>>a;
+        std::cout<<"Enter the second number: ";
+        std::cin>>b;
+        char op;
+        std::cout<<"enter operation(+,-,*,/): ";
+        std::cin>>op;
+        switch(op){
+            case '+':
+            std::cout<<"the sum is: "<< a+b<<'\n';
+            break;
+            case '*':
+            std::cout<<"the sum is: "<< a*b<<'\n';
+            break;
+            case '-':
+            std::cout<<"the sum is: "<< a-b<<'\n';
+            break;
+            case '/':
+            std::cout<<"the sum is: "<< a/b<<'\n';
+            break;
+            defaut:
+            std::cout<<"invalid operation"<<'\n';
+        }
+    std::cout<<"do you want to continue? (y/n): ";
+    char choice;
+    std::cin>>choice;
+    if(choice=='n' || choice=='N'){
         break;
-        case '*':
-        std::cout<<"the sum is: "<< a*b<<'\n';
-        break;
-        case '-':
-        std::cout<<"the sum is: "<< a-b<<'\n';
-        break;
-        case '/':
-        std::cout<<"the sum is: "<< a/b<<'\n';
-        break;
-        defaut:
-        std::cout<<"invalid operation"<<'\n';
+    
     }
+}
+
     return 0;
 }
--- hypotenuos.cpp
+++ hypotenuos.cpp
@@ -7,6 +7,6 @@ int main(){
     std::cout<< "Enter the Base: ";
     std::cin>> B;
     double hypo=sqrt(pow(H,2)+pow(B,2));
-    std::cout<< "The Hypotenous is: "<< hypo<< "cm"<< "\n";
+    std::cout<< "The Hypotenous is "<< hypo<< "cm"<< "\n";
     return 0;
 }
